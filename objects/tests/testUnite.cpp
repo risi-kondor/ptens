8799b256418c6bb8eb24efb22f73fc8632d154e6
Risi Kondor <risi@cs.uchicago.edu>
2022-10-18 18:09:28 -0500

    'changes'

---
+++ objects/tests/testUnite.cpp
@@ -0,0 +1,23 @@
+#include "Cnine_base.cpp"
+#include "CnineSession.hpp"
+#include "MsgFunctions.hpp"
+
+using namespace ptens;
+using namespace cnine;
+
+
+int main(int argc, char** argv){
+
+  cnine_session session;
+
+  const int n=8;
+  Hgraph G=Hgraph::random(n,0.3);
+  cout<<G<<endl;
+
+  Ptensors0 x=Ptensors0::sequential(n,2);
+  cout<<x<<endl;
+
+  cout<<unite1(x,G)<<endl;
+  
+
+}
