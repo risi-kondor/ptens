66d57cb5397cb53398d9c5a1b0cbd38d751d775c
Risi Kondor <risi@cs.uchicago.edu>
2022-10-30 14:47:23 -0500

    'changes'

--- python/bindings/OuterFunctions_py.cpp
+++ python/bindings/OuterFunctions_py.cpp
@@ -13,6 +13,8 @@ m.def("outer",[](const Ptensors1& x, const Ptensors0& y){return outer(x,y);});
 m.def("add_outer",[](Ptensors1& r, const Ptensors1& x, const Ptensors0& y){return add_outer(r,x,y);});
 m.def("add_outer_back0",[](Ptensors1& xg, const Ptensors1& g, const Ptensors0& y){return add_outer_back0(xg,g,y);});
 m.def("add_outer_back1",[](Ptensors0& yg, const Ptensors1& g, const Ptensors1& x){return add_outer_back0(yg,g,x);});
+m.def("add_outer_back0",[](loose_ptr<Ptensors1>& xg, const loose_ptr<Ptensors1>& g, const Ptensors0& y){return add_outer_back0(xg,g,y);});
+m.def("add_outer_back1",[](loose_ptr<Ptensors0>& yg, const loose_ptr<Ptensors1>& g, const Ptensors1& x){return add_outer_back0(yg,g,x);});
 
 m.def("outer",[](const Ptensors1& x, const Ptensors1& y){return outer(x,y);});
 m.def("add_outer",[](Ptensors2& r, const Ptensors1& x, const Ptensors1& y){return add_outer(r,x,y);});
