029f3f49da3eb338ec09dbfe7d21bf5963914f26
Andrew Hands <andrewrhands@gmail.com>
2022-11-11 16:03:59 -0600

    added a few tests

---
+++ python/manual_tests/unite1_cuda_degree_test.py
@@ -0,0 +1,13 @@
+import torch
+import ptens as p
+
+N = 200
+adj_matrix = torch.zeros(N,N,dtype=torch.float32)
+adj_matrix[:,0] = 1
+adj_matrix[0,:] = 1
+adj_matrix[0,0] = 0
+
+G = p.graph.from_matrix(adj_matrix)
+x = p.ptensors1.randn(G.nhoods(1),4)
+x = x.to('cuda')
+x = p.unite1(x,G)
