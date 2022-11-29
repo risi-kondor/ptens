8c2ca894843da7dec14b89cf48f26e9973db4383
Risi Kondor <risi@cs.uchicago.edu>
2022-11-29 14:14:30 -0600

    'changes'

--- python/bindings/AtomsPack_py.cpp
+++ python/bindings/AtomsPack_py.cpp
@@ -5,6 +5,9 @@ pybind11::class_<AtomsPack>(m,"atomspack")
   .def_static("from_list",[](const vector<vector<int> >& x){return AtomsPack(x);})
   .def_static("random",[](const int n, const float p){return AtomsPack::random(n,p);})
 
+  .def("__len__",&AtomsPack::size)
+  .def("__getitem__",[](const AtomsPack& x, const int i){return vector<int>(x[i]);})
+
   .def("str",&AtomsPack::str,py::arg("indent")="")
   .def("__str__",&AtomsPack::str,py::arg("indent")="")
   .def("__repr__",&AtomsPack::str,py::arg("indent")="");
