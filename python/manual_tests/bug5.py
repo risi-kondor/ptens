65198ee624115c381627defab210478005d294fc
Risi <risi@cs.uchicago.edu>
2022-12-08 00:57:30 -0600

    'changes'

--- python/manual_tests/bug5.py
+++ python/manual_tests/bug5.py
@@ -4,10 +4,12 @@ import ptens
 in_channels = 512
 out_channels = 256
 N = 60
+dev='cuda'
 
 G = ptens.graph.random(N,0.05)
-x = ptens.ptensors1.randn(G.nhoods(1),in_channels).to('cuda')
-w = torch.rand(in_channels,out_channels,dtype=torch.float32,device='cuda')
-b = torch.rand(out_channels,dtype=torch.float32,device='cuda')
+x = ptens.ptensors1.randn(G.nhoods(1),in_channels).to(dev)
+w = torch.rand(in_channels,out_channels,dtype=torch.float32,device=dev)
+b = torch.rand(out_channels,dtype=torch.float32,device=dev)
 while True:
+  print("\n")
   ptens.linear(x,w,b)
