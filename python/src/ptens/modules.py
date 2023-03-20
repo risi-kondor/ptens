ca4a2475b33b3340263bd07b7aa90b878cd35df8
Andrew Hands <andrewrhands@gmail.com>
2023-03-20 14:34:05 -0500

    updates to various modules

--- python/src/ptens/modules.py
+++ python/src/ptens/modules.py
@@ -1,26 +1,45 @@
-from typing import Callable, List, Optional, Tuple, Union
+from typing import List, Optional, Tuple, Union
 import torch
 import torch.nn as nn
-from torch.nn import Parameter, Sequential, LeakyReLU, ReLU
+from torch.nn import LeakyReLU
+from torch.nn.parameter import Parameter
 from torch.nn.functional import softmax, dropout
 import ptens 
 from ptens import ptensors0, ptensors1, ptensors2, graph
 from warnings import warn
 from ptens_base import atomspack
-from ptens.functions import linear, linmaps0, outer, unite1, gather, relu, cat
+from ptens.functions import linear, linmaps0, outer, unite1, unite2, gather, relu, cat
 ######################################## Functions ###########################################
 def ConvertEdgeAttributesToPtensors1(edge_index: torch.Tensor, edge_attributes: torch.Tensor):
   atoms = edge_index.transpose(0,1).float()
   return ptensors1.from_matrix(edge_attributes,atoms)
 
-def ComputeSubstructureMap(source_domains: atomspack, graph_filter: graph, G: graph, both_dirs: bool = False) -> Union[graph,Tuple[graph,graph]]:
+class MapInfo:
+  r"""
+    Boiler Plate class for storing information regarding a map between subgraphs.
+  """
+  def __init__(self, forward_map: Optional[atomspack], backward_map: Optional[atomspack], source_domains: Optional[atomspack], target_domains: Optional[atomspack], subgraph: Optional[graph]):
+    self.forward_map = forward_map
+    self.backward_map = backward_map
+    self.source_domains = source_domains
+    self.target_domains = target_domains
+    self.subgraph = subgraph
+def ComputeSubstructureMap(source_domains: atomspack, graph_filter: graph, G: graph, extra_info: bool = False, include_subgraph_graph: bool = False) -> Union[MapInfo,None]:
+  r"""
+  Returns None if subgraph does not appear
+  """
   # TODO: make it so .get_atoms() returns an atomspack, so we don't have to convert back to one.
   subgraphs = G.subgraphs(graph_filter)
-  to_sub = graph.overlaps(subgraphs,source_domains)
-  if both_dirs:
-    from_sub = graph.overlaps(source_domains,subgraphs)
-    return to_sub, from_sub
-  return to_sub
+  if len(subgraphs) == 0:
+    return None
+  info = [graph.overlaps(subgraphs,source_domains)]
+  if extra_info:
+    info.extend([
+      graph.overlaps(source_domains,subgraphs),
+      source_domains,
+      subgraphs,
+    ])
+  return MapInfo(*info,subgraph=graph.overlaps(subgraphs,subgraphs) if include_subgraph_graph else None)
 
 ######################################## MODULES ###########################################
 class Linear(torch.nn.Module):
@@ -51,18 +70,21 @@ class LazyLinear(torch.nn.Module):
   def reset_parameters(self):
     if not isinstance(self.w,torch.nn.parameter.UninitializedParameter):
       self.w = torch.nn.init.xavier_uniform_(self.w)
-      if not self.b is None:
+      if self.b is not None:
         self.b = torch.nn.init.zeros_(self.b)
   def initialize_parameters(self, input) -> None:
     if isinstance(self.w,torch.nn.parameter.UninitializedParameter):
+      assert self.out_channels is not None, "Cannot initialize parameters for LazyLinear layer when 'out_channels' is None."
       with torch.no_grad():
-        self.weight.materialize((self.out_channels, input.get_nc()))
-        if self.bias is not None:
-            self.bias.materialize(self.out_channels)
+        self.w.materialize((input.get_nc(),self.out_channels))
+        if self.b is not None:
+            self.b.materialize(self.out_channels)# type: ignore
         self.reset_parameters()
   def forward_standard(self,x: Union[ptensors0,ptensors1,ptensors2]) -> Union[ptensors0,ptensors1,ptensors2]:
     assert x.get_nc() == self.w.size(0)
     return x * self.w if self.b is None else linear(x,self.w,self.b)
+    # TODO: figure out why multiplication is broken. (nevermind, keeping this just in case)
+    #return linear(x,self.w,torch.zeros(self.w.size(0),device=self.w.device) if self.b is None else self.b)
   def forward(self,x: Union[ptensors0,ptensors1,ptensors2]) -> Union[ptensors0,ptensors1,ptensors2]:
     self.initialize_parameters(x)
     self.forward = self.forward_standard
@@ -102,7 +124,7 @@ def generate_generic_shape(name: str, size: int) -> graph:
   generate_path = lambda size: torch.tensor([[i,i + 1,i + 1,i] for i in range(size - 1)],dtype=torch.float32)
   edge_index = [generate_star,generate_cycle,generate_path][shape_types.index(name)](size)
   edge_index = edge_index.view(-1,2).transpose(0,1)
-  return graph.from_edge_index(edge_index,0)
+  return graph.from_edge_index(edge_index)
   #return graph.from_matrix(torch.sparse_coo_tensor(edge_index,torch.ones(edge_index.size(1),dtype=torch.float)).to_dense())
 def create_edge_ptensors1(edge_index: torch.Tensor, edge_attributes: torch.Tensor) -> ptensors1:
   atoms = edge_index.transpose(0,1).tolist()
@@ -138,7 +160,6 @@ class LazyUnite(torch.nn.Module):
     r"""
     reduction_types: "sum" and "mean"
     leave 'out_order' and 'channels_out' as 'None' to keep same as input
-    NOTE: if you are planning on using the same source/target domains more than once, consider using a unite layer instead and computing the domain mapping separately.
     """
     super().__init__()
     assert reduction_type == "sum" or reduction_type == "mean"
@@ -148,10 +169,9 @@ class LazyUnite(torch.nn.Module):
     self.out_order = out_order
     self.unite = None
   def forward(self, features: Union[ptensors0,ptensors1,ptensors2], graph: graph) -> Union[ptensors0,ptensors1,ptensors2]:
-    domain_map = graph.overlaps(graph.subgraphs(self.graph_filter),atomspack(features.get_atoms()))
     if self.unite is None:
       if self.lin.out_channels is None:
-        self.lin.out_channels = self.features.get_nc()
+        self.lin.out_channels = features.get_nc()
       if isinstance(features,ptensors0):
         in_order = 0
       elif isinstance(features,ptensors1):
@@ -163,17 +183,20 @@ class LazyUnite(torch.nn.Module):
       out_order = in_order if self.out_order is None else self.out_order
       assert out_order is not None, "If 'in_order' is '0', then 'out_order' cannot be 'None'."
       #
+      #self.unite = [
+      #  [lambda x,y: x,ptensors0.unite1,ptensors0.unite2],
+      #  [ptensors1.linmaps0,ptensors1.unite1,ptensors1.unite2],
+      #  [ptensors2.linmaps0,ptensors2.unite1,ptensors2.unite2],
+      #  ][in_order][out_order]
       self.unite = [
-        [lambda x,y: x,ptensors0.unite1,ptensors0.unite2],
-        [ptensors1.linmaps0,ptensors1.unite1,ptensors1.unite2],
-        [ptensors2.linmaps0,ptensors2.unite1,ptensors2.unite2],
-        ][in_order][out_order]
+        linmaps0,unite1,unite2
+      ][out_order]
       if in_order == 0 and out_order == 0:
         warn("You have 'in_order = out_order = 0', this means the unite layer is simply a linear layer.  Did you intend this?")
       if out_order == 0:
         q = self.unite
         self.unite = lambda x,G,n: q(x,n)
-    F = self.unite(features,domain_map,self.use_mean)
+    F = self.unite(features,graph,self.use_mean)
     F = self.lin(F)
     return F
 class LazySubstructureTransport(LazyUnite):
@@ -181,7 +204,7 @@ class LazySubstructureTransport(LazyUnite):
     r"""
     reduction_types: "sum" and "mean"
     leave 'out_order' as 'None' to keep same as input (NOTE: cannot leave as default if input is of order 0.)
-    NOTE: if you are planning on using the same source/target domains more than once, consider using a unite layer instead and computing the domain mapping separately.
+    NOTE: if you are planning on using the same source/target domains more than once, consider using a unite/transfer layer instead and computing the domain mapping separately.
     """
     super().__init__(channels_out,out_order,bias,reduction_type)
     assert out_order != 0
@@ -189,9 +212,14 @@ class LazySubstructureTransport(LazyUnite):
       graph_filter = generate_generic_shape(*graph_filter)
     assert isinstance(graph_filter,graph)
     self.graph_filter = graph_filter
-  def forward(self, features: Union[ptensors0,ptensors1,ptensors2], graph: graph) -> Union[ptensors0,ptensors1,ptensors2]:
+  def forward(self, features: Union[ptensors0,ptensors1,ptensors2], graph: graph) -> Union[ptensors0,ptensors1,ptensors2,None]:
+    r"""
+    Returns 'None' if nothing is mapped to.
+    """
     domain_map = ComputeSubstructureMap(features.get_atoms(),self.graph_filter,graph)
-    return super().forward(features,domain_map)
+    if domain_map is None:
+      return None
+    return super().forward(features,domain_map.forward_map) # type: ignore
 class LazyTransfer(torch.nn.Module):
   def __init__(self, channels_out: Optional[int] = None, reduction_type : str = "sum", out_order: Optional[int] = None, bias : bool = True) -> None:
     r"""
@@ -228,8 +256,8 @@ class LazyTransfer(torch.nn.Module):
         ][in_order][out_order]
       if in_order == 0:
         q = self.transfer
-        self.transfer = lambda x,G,n: q(x,n)
-    F = self.transfer(features,graph,self.use_mean)
+        self.transfer = lambda x,t,G,n: q(x,G,n)
+    F = self.transfer(features,target_domains, graph, self.use_mean)
     F = self.lin(F)
     return F
 class LazyTransferNHoods(LazyTransfer):
@@ -260,16 +288,13 @@ class LazyPtensGraphConvolutional(LazyTransfer):
       self.lin2.out_channels = features.get_nc()
     return self.forward(features,graph)
 class Dropout(torch.nn.Module):
-  def __init__(self, prob: torch.float = 0.5, device : str = 'cuda') -> None:
+  def __init__(self, prob: float = 0.5) -> None: # type: ignore
     super().__init__()
     self.p = prob
-    self.device = device
-  def cuda(self, device = None):
-    self.device = device
-    return super().cuda(device)
-  def forward(self, x):
+  def forward(self, x, device):
+    # TODO: replace device with device from 'x'.
     if self.training:
-      dropout = 1/(1 - self.p)*(torch.rand(x.get_nc(),device=self.device) > self.p)
+      dropout = 1/(1 - self.p)*(torch.rand(x.get_nc(),device=device) > self.p) # type: ignore
       if isinstance(x,ptensors0):
         return ptensors0.mult_channels(x,dropout)
       elif isinstance(x,ptensors1):
@@ -281,43 +306,47 @@ class Dropout(torch.nn.Module):
     else:
       return x
 class LazyBatchNorm(torch.nn.Module):
-  def __init__(self, eps: torch.float = 1E-5, momentum: torch.float = 0.1) -> None:
+  def __init__(self, eps: float = 1E-5, momentum: float = 0.1) -> None: # type: ignore
     super().__init__()
     #self.running_mean = torch.nn.parameter.UninitializedParameter()
     #self.running_var = torch.nn.parameter.UninitializedParameter()
-    #self.weight = torch.nn.parameter.UninitializedParameter()
-    #self.bias = torch.nn.parameter.UninitializedParameter()
+    self.weight = torch.nn.parameter.UninitializedParameter(requires_grad=True)
+    self.bias = torch.nn.parameter.UninitializedParameter(requires_grad=True)
     self.eps = eps
     self.momentum = momentum
+    self.first_run = True
   def forward(self, x):
     r"""
     x can be any type of ptensors
     """
     x_val : torch.Tensor = x.torch()
-    if len(list(self.parameters())) == 0:
-      nc = x.get_nc()
-      #self.running_mean.materialize(nc)
-      #self.running_var.materialize(nc)
-      #self.weight.materialize(nc)
-      #self.bias.materialize(nc)
-      running_mean = x_val.mean(0)
-      running_var = x_val.var(0)
-      self.running_mean = torch.nn.parameter.Parameter(running_mean)
-      self.running_var = torch.nn.parameter.Parameter(running_var)
-      self.weight = torch.nn.parameter.Parameter(torch.ones(nc))
-      self.bias = torch.nn.parameter.Parameter(torch.zeros(nc))
+    if self.training:
+      x_mean = x_val.mean(0)
+      x_var = x_val.var(0)
+      if self.first_run:
+        self.first_run = False
+        nc = x.get_nc()
+        self.weight.materialize(nc,device=x_val.device)
+        self.bias.materialize(nc,device=x_val.device)
+        self.register_buffer("running_mean",x_mean)
+        self.register_buffer("running_var",x_var)
+      else:
+        m = self.momentum
+        self.running_mean = (1 - m) * self.running_mean + m * x_mean
+        self.running_var = (1 - m) * self.running_var + m * x_var
+    elif self.running_mean is None:
+      return x # Why would you do this...
     else:
-      m = self.momentum
-      running_mean = (1 - m) * self.running_mean + m * x_val.mean(0)
-      running_var = (1 - m) * self.running_var + m * x_val.var(0)
+      x_mean = self.running_mean
+      x_var = self.running_var
     #
-    m = self.weight * (running_var + self.eps)**-0.5
+    mult = self.weight / torch.sqrt(x_var + self.eps)
     # TODO: if we can add channel wise addition broadcasting to all reference domains, we will not need to do this.
-    b = self.bias - running_mean * m
-    output = linear(x,torch.diag(m),b)
+    b = self.bias - x_mean * mult
+    output = linear(x,torch.diag(mult),b)
     return output
 class PNormalize(torch.nn.Module):
-  def __init__(self, p: int = 2, eps: torch.float = 1E-5) -> None:
+  def __init__(self, p: int = 2, eps: float = 1E-5) -> None:
     super().__init__()
     self.eps = eps
     self.p = p
