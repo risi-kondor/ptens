1c65b935511542508e4c69473427ca134be70842
Risi Kondor <risi@cs.uchicago.edu>
2023-12-25 23:07:08 -0600

    'changes'

---
+++ layers/tests3/testPtensors1g.cpp
@@ -0,0 +1,60 @@
+/*
+ * This file is part of ptens, a C++/CUDA library for permutation 
+ * equivariant message passing. 
+ *  
+ * Copyright (c) 2023, Imre Risi Kondor
+ *
+ * This source code file is subject to the terms of the noncommercial 
+ * license distributed with cnine in the file LICENSE.TXT. Commercial 
+ * use is prohibited. All redistributed versions of this file (in 
+ * original or modified form) must retain this copyright notice and 
+ * must be accompanied by a verbatim copy of the license. 
+ *
+ */
+#include "Cnine_base.cpp"
+#include "CnineSession.hpp"
+
+#include "Ptensors0b.hpp"
+#include "Ptensors1b.hpp"
+#include "Ptensors2b.hpp"
+
+using namespace ptens;
+using namespace cnine;
+
+PtensSession ptens_session;
+
+
+int main(int argc, char** argv){
+
+  typedef Ptensors0b<float> Ptens0;
+  typedef Ptensors1b<float> Ptens1;
+  typedef Ptensors2b<float> Ptens2;
+
+  AtomsPack xatoms=AtomsPack::random(4,0.5);
+  Ptens0 X0=Ptens0(xatoms,channels=3,filltype=3);
+  Ptens1 X1=Ptens1(xatoms,channels=3,filltype=3);
+  Ptens2 X2=Ptens2(xatoms,channels=3,filltype=3);
+
+  Ptens0 X0g(X0,1); 
+  Ptens1 X1g(X1,1); 
+  Ptens2 X2g(X2,1); 
+
+  AtomsPack yatoms=AtomsPack::random(4,0.5);
+  cout<<X0<<endl;
+  Ptens1 Y0=Ptens1::gather(X0,yatoms);
+  Ptens1 Y1=Ptens1::gather(X1,yatoms);
+  Ptens1 Y2=Ptens1::gather(X1,yatoms);
+
+  Ptens1 Y0g=Ptens1::gather(X0g,yatoms);
+  Ptens1 Y1g=Ptens1::gather(X1g,yatoms);
+  Ptens1 Y2g=Ptens1::gather(X1g,yatoms);
+
+  cout<<Y0<<endl;
+  cout<<Y0g<<endl;
+  cout<<Y1<<endl;
+  cout<<Y1g<<endl;
+  cout<<Y2<<endl;
+  cout<<Y2g<<endl;
+
+}
+
