d778603142d71cbbcefd19ac8e7d5a8ef2b4933f
Risi Kondor <risi@cs.uchicago.edu>
2024-08-27 15:38:07 -0500

    'changes'

---
+++ python/src/ptens/csubgraphlayer2.py
@@ -0,0 +1,95 @@
+#
+# This file is part of ptens, a C++/CUDA library for permutation 
+# equivariant message passing. 
+#  
+# Copyright (c) 2023, Imre Risi Kondor
+#
+# This source code file is subject to the terms of the noncommercial 
+# license distributed with cnine in the file LICENSE.TXT. Commercial 
+# use is prohibited. All redistributed versions of this file (in 
+# original or modified form) must retain this copyright notice and 
+# must be accompanied by a verbatim copy of the license. 
+#
+#
+
+import torch
+
+import ptens as p
+import ptens_base as pb 
+
+import ptens.cptensorlayer2 as cptensorlayer2
+
+
+class csubgraphlayer2(p.csubgraphlayer,cptensorlayer2):
+
+
+    def __new__(cls,G,S,atoms,M):
+        assert isinstance(atoms,pb.catomspack)
+        assert isinstance(G,p.ggraph)
+        assert isinstance(S,p.subgraph)
+        R=super().__new__(csubgraphlayer2,M)
+        R.atoms=atoms
+        R.G=G
+        R.S=S
+        return R
+
+    @classmethod
+    def zeros(self,G,S,nvecs,nc,device='cpu'):
+        assert isinstance(G,p.ggraph)
+        assert isinstance(S,p.subgraph)
+        S.set_evecs()
+        atoms=pb.csubgraphatoms_cache.subgraphs(G.obj,S.obj,nvecs)
+        M=torch.zeros([len(atoms),nvecs,nvecs,nc],device=device)
+        return csubgraphlayer2(G,S,atoms,M)
+
+    @classmethod
+    def randn(self,G,S,nvecs,nc,device='cpu'):
+        assert isinstance(G,p.ggraph)
+        assert isinstance(S,p.subgraph)
+        S.set_evecs()
+        atoms=pb.csubgraphatoms_cache.subgraphs(G.obj,S.obj,nvecs)
+        M=torch.randn([len(atoms),nvecs,nvecs,nc],device=device)
+        return csubgraphlayer2(G,S,atoms,M)
+
+    @classmethod
+    def from_tensor(self,G,S,M):
+        assert isinstance(G,p.ggraph)
+        assert isinstance(S,p.subgraph)
+        assert isinstance(M,torch.Tensor)
+        assert M.dim()==3
+        S.set_evecs()
+        atoms=pb.csubgraphatoms_cache.subgraphs(G.obj,S.obj,M.size(1))
+        return csubgraphlayer2(G,S,atoms,M)
+
+    
+    # ---- Linmaps ------------------------------------------------------------------------------------------
+
+
+    @classmethod
+    def linmaps(self,x):
+        return csubgraphlayer2(x.G,x.S,x.atoms,super().linmaps(x))
+
+
+    # ---- Gather ------------------------------------------------------------------------------------------
+
+
+    @classmethod
+    def gather(self,S,x):
+        atoms=pb.csubgraphatoms_cache.subgraphs(x.G.obj,S.obj,x.get_nvecs())
+        return csubgraphlayer2(x.G,x.S,atoms,super().gather(atoms,x))
+
+
+    # ---- I/O ----------------------------------------------------------------------------------------------
+
+
+    def __repr__(self):
+        return "csubgraphlayer2(len="+str(self.size(0))+",nvecs="+str(self.size(1))+",nc="+str(self.get_nc())+")"
+
+    def __str__(self,indent=""):
+        r=indent+"csubgraphlayer2:\n"
+        r=r+self.backend().__str__(indent+"  ")
+        return r
+
+
+
+
---
+++ python/tests3/test_csubgraphlayer1.py
@@ -0,0 +1,74 @@
+import torch
+import ptens_base as pb
+import ptens as p
+
+print("\n-------------------------------------")
+print (" csubgraphlayer1")
+print("-------------------------------------\n")
+
+print("A compressed subgraphlayer1 is a first order P-tensor layer corresponding\nto the subgraphs induced by S in G.\n\n")
+nvecs=3
+
+print("We can define a random first order subgraph layer:\n")
+G=p.ggraph.random(8,0.5)
+S=p.subgraph.triangle()
+A=p.csubgraphlayer1.randn(G,S,nvecs,3)
+print(A)
+
+print("Or define it from a torch tensor:\n")
+M=torch.randn([G.subgraphs(S).nrows1(),nvecs,3])
+A=p.csubgraphlayer1.from_tensor(G,S,M)
+print(A)
+
+print("If two subgraphlayers have the same reference domains,\nwe can do arithmetic on them:\n")
+B=p.csubgraphlayer1.randn(G,S,nvecs,3)
+print(A+A)
+
+
+print("\n---------")
+print(" Linmaps")
+print("---------\n")
+
+#print("Linmaps from a 0th order subgraph layer:\n")
+#A=p.csubgraphlayer0.randn(G,S,nvecs,3)
+#B=p.csubgraphlayer0.linmaps(A)
+#print(B)
+
+print("Linmaps from a 1st order subgraph layer:\n")
+A=p.csubgraphlayer1.randn(G,S,nvecs,3)
+B=p.csubgraphlayer1.linmaps(A)
+print(B)
+
+print("Linmaps from a 2nd order subgraph layer:\n")
+A=p.csubgraphlayer2.randn(G,S,nvecs,3)
+B=p.csubgraphlayer1.linmaps(A)
+print(B)
+
+print("\n---------")
+print(" Gather")
+print("---------\n")
+
+E=p.subgraph.edge()
+
+
+#print("Gather from a 0th order subgraph layer:\n")
+#A0=p.csubgraphlayer0.randn(G,E,nvecs,3)
+#B0=p.csubgraphlayer1.gather(S,A0)
+#print(B0)
+
+print("Gather from a 1st order subgraph layer:\n")
+A1=p.csubgraphlayer1.randn(G,S,3,3)
+B1=p.csubgraphlayer1.gather(S,A1)
+print(B1)
+
+
+print("Gather from a 2nd order subgraph layer:\n")
+A2=p.csubgraphlayer2.randn(G,S,nvecs,3)
+B2=p.csubgraphlayer1.gather(S,A2)
+print(B2)
+
+P=pb.ggraph_preloader(G.obj)
+print(P)
+print("\n")
+
+
