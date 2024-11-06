45fdd98150a420397bc158a9a7b457ee17d94b29
Ludwig Schneider <ludwigschneider@uchicago.edu>
2024-11-06 14:23:31 -0600

    ruff format

--- python/pytest/conftest.py
+++ python/pytest/conftest.py
@@ -2,9 +2,11 @@ import os
 
 import pytest
 
+
 @pytest.fixture(scope="session")
 def ptens_cuda_support():
     import ptens_base
+
     string = ptens_base.status_str().split("\n")
     for line in string:
         if "CUDA support" in line:
@@ -14,17 +16,19 @@ def ptens_cuda_support():
                 return False
     assert False
 
+
 @pytest.fixture(scope="session")
 def device(ptens_cuda_support):
-    
     device = os.environ["TORCH_TEST_DEVICE"]
 
     if "cuda" in device:
         assert ptens_cuda_support
         import torch
+
         assert torch.cuda.is_available()
 
     return device
+
 
 @pytest.fixture(scope="session")
 def float_epsilon():
--- python/pytest/test_tests.py
+++ python/pytest/test_tests.py
@@ -3,15 +3,15 @@ import ptens
 from ptens_base import atomspack
 from torch.autograd.gradcheck import gradcheck, gradgradcheck
 
+
 def test_bug1(device):
     nnodes = 15
-    graph =ptens.ggraph.random(nnodes,0.5)
+    graph = ptens.ggraph.random(nnodes, 0.5)
     print(graph)
     subgraphs = [ptens.subgraph.trivial(), ptens.subgraph.edge()]
     node_values = torch.rand(nnodes, 1, requires_grad=True)
 
     node_attributes = ptens.subgraphlayer0.from_matrix(graph, ptens.subgraph.trivial(), node_values)
-
 
     for sg in subgraphs:
         gather_features = ptens.subgraphlayer0.gather(sg, node_attributes)
@@ -26,4 +26,3 @@ def test_bug1(device):
 
         check = gradcheck(ptens.subgraphlayer0.gather, (sg, node_attributes), eps=1e-3)
         print(check)
-
