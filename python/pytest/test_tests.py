2d4756a5f46f877bdefc07b4bd09a9841ba0570d
Ludwig Schneider <ludwigschneider@uchicago.edu>
2024-11-07 11:05:01 -0600

    inter

--- python/pytest/test_tests.py
+++ python/pytest/test_tests.py
@@ -9,7 +9,6 @@ from torch.autograd.gradcheck import gradcheck
 def test_bug1(device):
     nnodes = 15
     graph = ptens.ggraph.random(nnodes, 0.5)
-    print(graph)
     subgraphs = [ptens.subgraph.trivial(), ptens.subgraph.edge()]
     node_values = torch.rand(nnodes, 1, requires_grad=True)
 
@@ -19,41 +18,57 @@ def test_bug1(device):
         gather_features = ptens.subgraphlayer0.gather(sg, node_attributes)
         result = torch.sum(gather_features)
         result.backward()
-        print(node_values.grad)
 
         # linmap_features = ptens.subgraphlayer0.linmaps(node_attributes)
         result = torch.sum(node_attributes)
         result.backward()
-        print(node_attributes.grad)
 
         check = gradcheck(ptens.subgraphlayer0.gather, (sg, node_attributes), eps=1e-3)
-        print(check)
+        assert check
 
 
 
 class TestGather(object):
 
-    def backprop(self,cls,fn,N,_nc):
-        if(cls==ptens.ptensor0):
-            x=cls.randn(N,_nc)
-        else:
-            atoms=ptens_base.atomspack.random(N,0.3)
-            x=cls.randn(atoms,_nc)
+    
+    def backprop(self,cls, N,nc, device):
+        atoms=ptens_base.atomspack.random(N, nc, 0.3)
+        x=cls.randn(atoms,nc).to(device)
         x.requires_grad_()
         G=ptens.ggraph.random(N,0.3)
-        z=fn(x,G)
+        atoms2 = G.subgraphs(ptens.subgraph.trivial())
+
+        check = gradcheck(cls.gather, (atoms2, x), eps=1e-3)
+        assert check
+
+        z = cls.gather(atoms2, x)
+        loss=torch.sum(z)
+        loss.backward()
+        xgrad=x.grad
+        print("xgrad", xgrad)
+
+
+        h=1e-6
+        xgrad2 = torch.zero_like(xgrad)
+        for i in range(xgrad2.size()):
+            xp = copy.deepcopy(x)
+            xm = copy.deepcopy(x)
+            xp[i] += h
+            xm[i] -= h
+            
+            grad[i] = cls(gather
+
+        z_plus = cls.gather(atoms2, x+h)
+        z_minus = cls.gather(atoms2, x-h)
+        xgrad2 = (z_plus - z_minus)/(2*h)
+        print("xgrad2", xgrad2)
         
-        testvec=z.randn_like()
-        loss=z.inp(testvec).to('cuda')
-        loss.backward(torch.tensor(1.0))
-        xgrad=x.get_grad()
 
-        xeps=x.randn_like()
-        z=fn(x+xeps,G)
-        xloss=z.inp(testvec).to('cuda')
-        assert(torch.allclose(xloss-loss,xeps.inp(xgrad),rtol=1e-3, atol=1e-4))
+        
+    @pytest.mark.parametrize(('N', 'nc'), [(8, 1), (1, 2), (16, 4)])
+    def test_gather0(self,N, nc, device):
+        self.backprop(ptens.ptensorlayer0,N,nc, device)
 
-
-    @pytest.mark.parametrize('nc', [1, 2, 4])
-    def test_gather(self,nc):
-        self.backprop(ptens.ptensor0,ptens.gather,8,nc)
+    @pytest.mark.parametrize(('N', 'nc'), [(8, 1), (1, 2), (16, 4)])
+    def test_gather1(self,N, nc, device):
+        self.backprop(ptens.ptensorlayer0,N,nc, device)
