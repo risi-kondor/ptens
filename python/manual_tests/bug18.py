fbc5a8040a72a1b96ad36fc6d8009e5218c0f12f
Risi <risi@cs.uchicago.edu>
2024-11-20 23:56:24 -0600

    'changes'

--- python/manual_tests/bug18.py
+++ python/manual_tests/bug18.py
@@ -6,7 +6,7 @@ G = ptens.ggraph.random(3, 1.0)
 atoms_in = G.subgraphs(ptens.subgraph.triangle())
 atoms_out = G.subgraphs(ptens.subgraph.trivial())
 
-nc = 2
+nc = 1
 x = ptens.ptensorlayer2.sequential(atoms_in, nc) + 2
 x = x.to('cuda')
 x.requires_grad_()
@@ -14,6 +14,7 @@ x.requires_grad_()
 z = ptens.ptensorlayer2.gather(atoms_out, x)
 zero = z.zeros_like()
 print(z.device, zero.device)
+print(z)
 
 l1sum = torch.nn.L1Loss(reduction='sum')
 def reduction_fn(z):
