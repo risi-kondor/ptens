96a04c4ccd8adecc699e5601389796dcc4ecd204
Ludwig Schneider <ludwigschneider@uchicago.edu>
2024-11-21 09:27:32 -0600

    revert stack protector

--- python/setup.py
+++ python/setup.py
@@ -83,7 +83,6 @@ def main():
                          '-DCNINE_DEVICE_CHECKING',
                          '-DWITH_FAKE_GRAD',
                          '-DCNINE_FUNCTION_TRACING',
-                         '-fstack-protector',
                          ]
 
     _nvcc_compile_args = ['-D_WITH_CUDA',
